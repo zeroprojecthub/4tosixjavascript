cbaef3803d4c1c8147374b46576846dcf3f2c19a
PD <zeroprojecthub@gmail.com>
2024-03-27 17:10:51 +0530

    First commit-html,css and js file added

---
+++ app.js
@@ -0,0 +1,78 @@
+
+// const numc=44;
+// // num1=23;
+// numv=11;
+// {
+//     var numv=33;
+//     let num1=55;
+//     console.log('number1::',num1);
+
+// }
+// console.log('numberc::',numc);
+// console.log('numberv::',numv);
+
+
+
+// if(1)
+// {
+//     console.log("mornning")
+// }else{
+//     console.log("evening");
+// }
+
+
+// const arr=[22,33,44,5];
+// arr.push(344);
+// console.log(arr);
+
+
+// const person={
+//     name:"pankaj",
+//     age:34
+// }
+
+// person.name = 'John';
+// person.email = 'brad@gmail.com';
+// console.log(person);
+
+
+// let n=2323;
+// n="rahul";
+// n=true;
+// n=null;
+// n=undefined;
+// n=9007199254740991n;
+// // let nn;
+// n=[22,33,44,55];
+// n={
+//     name:"rahul"
+// }
+
+// function sayHello() {
+//     console.log('Hello');
+//   }
+
+// n=sayHello;
+
+// console.log(typeof n,n);
+
+// const name = 'John';
+
+// let newName = name;
+// newName = 'Jonathan';
+
+// console.log('name:::',name);
+// console.log('newname:::',newName);
+const person = {
+    name: 'Brad',
+    age: 40,
+  };
+
+  let newPerson = person;
+  newPerson.name = 'Bradley';
+console.log(person);
+
+console.log(newPerson);
+
+
+
