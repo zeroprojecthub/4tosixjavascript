ecbec8b3859269fc464c2b75ccff835a0430e7a9
zeroprojecthub <143165786+zeroprojecthub@users.noreply.github.com>
2024-04-22 09:05:38 +0530

    DOM PART OF JAVASCRIPT
    document object model

---
+++ 06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script1.js
@@ -0,0 +1,33 @@
+function createNewItem(item) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(item));
+
+    const button = createButton('remove-item btn-link text-red');
+    li.appendChild(button);
+
+    document.querySelector('.items').appendChild(li);
+
+}
+
+function createButton(classes) {
+
+    const button = document.createElement('button');
+    button.className = classes;
+
+    const icon = createIcon('fa-solid fa-xmark');
+
+    button.appendChild(icon);
+
+    return button;
+}
+
+function createIcon(classes) {
+    const icon = document.createElement('i');
+    icon.className = classes;
+
+    return icon;
+}
+
+
+createNewItem('Rose Berrey');
+createNewItem('Mango');
